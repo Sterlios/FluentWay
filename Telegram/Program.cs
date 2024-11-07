360409d3caac1ff56769a03bbcebe6294a2277da
Sterlios <kuzmin.s.anton@gmail.com>
2024-11-07 11:49:34 +0100

    Add DbContext to Services

--- Telegram/Program.cs
+++ Telegram/Program.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Telegram.Bot;
+using Telegram.DbContexts;
 
 namespace Telegram
 {
@@ -12,17 +14,10 @@ namespace Telegram
 
             hostBuilder.ConfigureServices((hostBuilder, services) =>
             {
-                services.Configure<BotConfiguration>(hostBuilder.Configuration.GetSection("BotConfiguration"));
-                services.AddHttpClient("telegram_bot_client").RemoveAllLoggers()
-                .AddTypedClient<ITelegramBotClient>((httpClient, sp) =>
+                services.AddDbContext<TelegramDbContext>(options =>
                 {
-                    BotConfiguration? botConfiguration = sp.GetService<IOptions<BotConfiguration>>()?.Value;
-                    ArgumentNullException.ThrowIfNull(botConfiguration);
-                    TelegramBotClientOptions options = new(botConfiguration.BotToken);
-                    return new TelegramBotClient(options, httpClient);
+                    options.UseSqlServer(hostBuilder.Configuration.GetConnectionString("TelegramDb"));
                 });
-                services.AddDbContext<UserContext>();
-                services.AddSingleton<ITelegramBotClient>();
             });
 
             var host = hostBuilder.Build();
